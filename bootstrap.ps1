b8329638ae5eaaf50c783f64e4e609c963790a11
Bradley Newman <bnewman227@gmail.com>
2022-12-21 21:58:21 +1030

    Update scripts

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,12 +1,13 @@
 Write-Output "Running bootstrap script..."
 $Shell = New-Object -ComObject "WScript.Shell"
-$Button = $Shell.Popup("Please ensure Windows 11 is up to date before continuing.", 0, "Windows Bootstrap script", 0)
+$Button = $Shell.Popup("Please ensure Windows 11 is up to date before continuing.`r`nThis script must NOT be run in an elevated shell.", 0, "Windows Bootstrap script", 0)
 
 #=========Install apps with package managers=========
 
 # Download applist-winget.txt from the bootstrap repository and install winget apps
 Write-Output "Downloading Winget App list..."
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/soda3x/windows-bootstrap/main/applist-winget.txt -OutFile .\applist-winget.txt
+Write-Output "Done."
 
 [string[]] $wingetApps = Get-Content -Path '.\applist-winget.txt'
 
@@ -15,29 +16,35 @@ foreach ($app in $wingetApps) {
     Write-Output "Installing $app..."
     Invoke-Expression "winget install $app"
 }
+Write-Output "Done."
 
 # Get and Install scoop package manager
 Write-Output "Installing Scoop Package Manager..."
 Invoke-RestMethod get.scoop.sh | Invoke-Expression
 
 Invoke-Expression "scoop bucket add extras"
+Write-Output "Done."
 
 # Download applist-scoop.txt from the bootstrap repository and install scoop apps
 Write-Output "Downloading Scoop App list..."
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/soda3x/windows-bootstrap/main/applist-scoop.txt -OutFile .\applist-scoop.txt
+Write-Output "Done."
 
 [string[]] $scoopApps = Get-Content -Path '.\applist-scoop.txt'
 
+Write-Output "Installing Scoop Apps..."
 foreach ($app in $scoopApps) {
     Write-Output "Installing $app..."
     Invoke-Expression "scoop install $app"
 }
+Write-Output "Done."
 
 #=========Install stand-alone apps that can't be installed via a pkg manager=========
 
 # Install pipx
 Write-Output "Installing Pipx..."
 Invoke-Expression "python3 -m pip install --user pipx"
+Write-Output "Done."
 
 # Use regex to get Python3.X path and keep this script future proof
 $python3Path = Get-ChildItem | Where-Object $_.Name -match {"$env:USERPROFILE\AppData\Roaming\Python\Python3*\Scripts"}
@@ -47,6 +54,7 @@ Invoke-Expression "cd $python3Path\pipx.exe ensurepath"
 # Install pls
 Write-Output "Installing pls..."
 Invoke-Expression "pipx install pls"
+Write-Output "Done."
 
 # Make C:\tools directory
 New-Item -Path "c:\" -Name "tools" -ItemType "directory"
@@ -54,6 +62,7 @@ New-Item -Path "c:\" -Name "tools" -ItemType "directory"
 # Install portal and place in C:\tools
 Write-Output "Installing Portal..."
 Invoke-WebRequest -Uri https://github.com/SpatiumPortae/portal/releases/download/v1.0.3/portal_1.0.3_Windows_x86_64.zip -OutFile C:\tools\portal.exe
+Write-Output "Done."
 
 
 # Alias commands in Powershell by creating a profile
@@ -72,6 +81,7 @@ Write-Output "Installing Neovim..."
 # Make nvim and autoload directories in Local AppData
 New-Item -Path $env:USERPROFILE\AppData\Local -Name "nvim" -ItemType "directory"
 New-Item -Path $env:USERPROFILE\AppData\Local\nvim -Name "autoload" -ItemType "directory"
+Write-Output "Done."
 
 Write-Output "Configuring Neovim..."
 # Download and place nvim config into newly created nvim folder
@@ -79,6 +89,7 @@ Invoke-WebRequest -Uri https://raw.githubusercontent.com/soda3x/windows-bootstra
 
 # Download vim-plug
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim  -OutFile $env:USERPROFILE\AppData\Local\nvim\autoload\plug.vim
+Write-Output "Done."
 
 # Run post-install commands
 Write-Output "Running post install commands..."
@@ -89,6 +100,7 @@ Invoke-WebRequest -Uri https://raw.githubusercontent.com/soda3x/windows-bootstra
 foreach ($cmd in $postInstallCmds) {
     Invoke-Expression "$cmd"
 }
+Write-Output "Done."
 
 # Finally clean up by removing files created by this script
 Write-Output "Cleaning up..."
@@ -96,6 +108,7 @@ Remove-Item '.\applist-winget.txt'
 Remove-Item '.\applist-scoop.txt'
 Remove-Item '.\applist-pip.txt'
 Remove-Item '.\post-install-commands.txt'
+Write-Output "Done."
 
 Write-Output "Finished."
 $Shell = New-Object -ComObject "WScript.Shell"
--- elevated-bootstrap.ps1
+++ elevated-bootstrap.ps1
@@ -3,6 +3,7 @@ Write-Output "Running elevated bootstrap..."
 # Turn off UAC
 Write-Output "Turning off UAC..."
 Set-ItemProperty -Path REGISTRY::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System -Name ConsentPromptBehaviorAdmin -Value 0
+Write-Output "Done."
 
 Write-Output "Configuring explorer (show hidden files / folders, protected OS files and file extensions)..."
 # Show Hidden Files, Protected OS Files and File Extensions in Explorer
@@ -11,26 +12,32 @@ Set-ItemProperty $key Hidden 1
 Set-ItemProperty $key HideFileExt 0
 Set-ItemProperty $key ShowSuperHidden 1
 Stop-Process -processname explorer
+Write-Output "Done."
 
 # Enable Hyper-V:
 Write-Output "Turning on Hyper-V..."
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-All -All -NoRestart
+Write-Output "Done."
 
 # Enable WSL:
 Write-Output "Turning on WSL..."
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -All -NoRestart
+Write-Output "Done."
 
 # Add C:\tools to system path, stand-alone apps will be installed to C:\tools and will need to be on path
 Write-Output "Adding C:\tools to the system path..."
-[System.Environment]::SetEnvironmentVariable('Path','$env:Path;C:\tools',[System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('Path','${env:Path};C:\tools',[System.EnvironmentVariableTarget]::Machine)
+Write-Output "Done."
 
 # Disable bing search results from start-menu search
 Write-Output "Disabling Bing search results from the start menu..."
 Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search -Name BingSearchEnabled -Type DWord -Value 0
+Write-Output "Done."
 
 # Change explorer home screen back to This PC
 Write-Output "Reverting Explorer Home screen back to 'This PC'..."
 Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name LaunchTo -Type DWord -Value 1
+Write-Output "Done."
 
 Write-Output "Finished running elevated bootstrap script..."
 $Shell = New-Object -ComObject "WScript.Shell"
