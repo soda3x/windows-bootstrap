fd3c7fe85bfaa2e23b66aa0c3919eb49ab28c761
Bradley Newman <bnewman227@gmail.com>
2023-06-20 12:14:12 +0930

    Update elevated-bootstrap.ps1

--- elevated-bootstrap.ps1
+++ elevated-bootstrap.ps1
@@ -45,7 +45,48 @@ Write-Output "Done."
 Write-Output "Uninstalling OneDrive..."
 Invoke-Expression "taskkill /f /im OneDrive.exe"
 Invoke-Expression "${env:SystemRoot}\system32\OneDriveSetup.exe /uninstall"
+# The following will stop it showing up in Explorer
+New-PSDrive -Name HKCR -PSProvider Registry -Root HKEY_CLASSES_ROOT
+Remove-Item -Path 'HKCR:\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}' -Recurse
+Remove-Item -Path 'HKCR:\Wow6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}' -Recurse
 Write-Output "Done."
+
+# Customise the Taskbar
+REG LOAD HKLM\Default C:\Users\Default\NTUSER.DAT
+
+Write-Output "Customising the Taskbar..."
+Write-Output "Removing Task View..."
+$reg = New-ItemProperty "HKLM:\Default\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "ShowTaskViewButton" -Value "0" -PropertyType Dword -Force
+try { $reg.Handle.Close() } catch {}
+
+Write-Output "Removing Chat..."
+$reg = New-ItemProperty "HKLM:\Default\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarMn" -Value "0" -PropertyType Dword -Force
+try { $reg.Handle.Close() } catch {}
+
+Write-Output "Removing Search..."
+$RegKey = "HKLM:\Default\Software\Microsoft\Windows\CurrentVersion\Search"
+if (-not(Test-Path $RegKey)) {
+  $reg = New-Item $RegKey -Force | Out-Null
+  try { $reg.Handle.Close() } catch {}
+}
+$reg = New-ItemProperty $RegKey -Name "SearchboxTaskbarMode"  -Value "0" -PropertyType Dword -Force
+try { $reg.Handle.Close() } catch {}
+
+Write-Output "Setting Start Menu to display More Pins..."
+$reg = New-ItemProperty "HKLM:\Default\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "Start_Layout" -Value "1" -PropertyType Dword -Force
+try { $reg.Handle.Close() } catch {}
+
+REG UNLOAD HKLM\Default
+
+# Debloat Windows
+Write-Output "Removing unwanted apps and adverts..."
+New-Item -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\' -Name 'CloudContent'
+New-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\CloudContent' -Name 'DisableWindowsConsumerFeatures' -PropertyType DWord -Value '1' -Force
+
+Write-Output "Disabling data collection and telemetry..."
+New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer' -Name 'SmartScreenEnabled' -PropertyType String -Value 'Off' -Force
+New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\DataCollection' -Name 'AllowTelemetry' -PropertyType DWord -Value '0' -Force
+New-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\DataCollection' -Name 'AllowTelemetry' -PropertyType DWord -Value '0' -Force
 
 Write-Output "Finished running elevated bootstrap script..."
 $Shell = New-Object -ComObject "WScript.Shell"
